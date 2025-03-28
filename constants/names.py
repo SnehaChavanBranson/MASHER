9dfd901f9b92adcf9776c39993a0861671efd1b5
SnehaChavanBranson <sneha.chavan@emerson.com>
2025-03-28 12:38:25 +0530

    [add] added constants and objects

---
+++ constants/names.py
@@ -0,0 +1,81 @@
+# encoding: UTF-8
+
+# Login Objects 
+
+uIController_QQuickWindowQmlImpl = {"title": "UIController", "type": "QQuickWindowQmlImpl", "unnamed": 1, "visible": True}
+uIController_loginFlick_Flickable = {"container": uIController_QQuickWindowQmlImpl, "id": "loginFlick", "type": "Flickable", "unnamed": 1, "visible": True}
+UserName_TextField = {"backgroundcolor": "#ffffff", "container": uIController_loginFlick_Flickable, "echoMode": 0, "id": "textFildUserName", "type": "TextField", "unnamed": 1, "visible": True}
+Password_TextField = {"backgroundcolor": "#ffffff", "container": uIController_loginFlick_Flickable, "echoMode": 2, "id": "textFildPassword", "passwordCharacter": "*", "type": "TextField", "unnamed": 1, "visible": True}
+login_button_obj = {"checkable": False, "container": uIController_loginFlick_Flickable, "id": "loginButton", "type": "Button", "unnamed": 1, "visible": True}
+uIController_OK_BransonPrimaryButton = {"checkable": False, "container": uIController_QQuickWindowQmlImpl, "id": "ok", "text": "OK", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+loginFlick_Branson_Polaris_Text = {"container": uIController_loginFlick_Flickable, "text": "Branson Polaris", "type": "Text", "unnamed": 1, "visible": True}
+uIController_backgroundLoginUi_BorderImage = {"container": uIController_QQuickWindowQmlImpl, "id": "backgroundLoginUi", "source": "qrc:/Images/Login_bg.svg", "type": "BorderImage", "unnamed": 1, "visible": True}
+uIController_loginFlick_Flickable_2 = {"container": uIController_QQuickWindowQmlImpl, "id": "loginFlick", "occurrence": 2, "type": "Flickable", "unnamed": 1, "visible": True}
+loginFlick_textFildCurrentPassword_TextField = {"backgroundcolor": "#ffffff", "container": uIController_loginFlick_Flickable_2, "echoMode": 2, "id": "textFildCurrentPassword", "passwordCharacter": "*", "type": "TextField", "unnamed": 1, "visible": True}
+loginFlick_textFildNewPassword_TextField = {"backgroundcolor": "#ffffff", "container": uIController_loginFlick_Flickable_2, "echoMode": 2, "id": "textFildNewPassword", "passwordCharacter": "*", "type": "TextField", "unnamed": 1, "visible": True}
+loginFlick_textFildConfirmPassword_TextField = {"backgroundcolor": "#ffffff", "container": uIController_loginFlick_Flickable_2, "echoMode": 2, "id": "textFildConfirmPassword", "passwordCharacter": "*", "type": "TextField", "unnamed": 1, "visible": True}
+uIController_backgroundChangePassUi_BorderImage = {"container": uIController_QQuickWindowQmlImpl, "id": "backgroundChangePassUi", "source": "qrc:/Images/Login_bg.svg", "type": "BorderImage", "unnamed": 1, "visible": True}
+loginFlick_resetButton_Button = {"checkable": False, "container": uIController_loginFlick_Flickable_2, "id": "resetButton", "type": "Button", "unnamed": 1, "visible": True}
+uIController_Rectangle = {"container": uIController_QQuickWindowQmlImpl, "occurrence": 68, "type": "Rectangle", "unnamed": 1, "visible": True}
+uIController_Overlay = {"container": uIController_QQuickWindowQmlImpl, "type": "Overlay", "unnamed": 1, "visible": True}
+imageCross_Image = {"container": uIController_Overlay, "id": "imageCross", "source": "qrc:/Images/crossMark.svg", "type": "Image", "unnamed": 1, "visible": True}
+uIController_popupBackground_Rectangle = {"container": uIController_QQuickWindowQmlImpl, "id": "popupBackground", "type": "Rectangle", "unnamed": 1, "visible": True}
+uIController_Rectangle_2 = {"container": uIController_QQuickWindowQmlImpl, "occurrence": 32, "type": "Rectangle", "unnamed": 1, "visible": True}
+uIController_User_Name_is_Empty_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "User Name is Empty", "type": "Text", "unnamed": 1, "visible": True}
+uIController_Password_is_Empty_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Password is Empty", "type": "Text", "unnamed": 1, "visible": True}
+uIController_Invalid_Username_or_Password_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Invalid Username or Password", "type": "Text", "unnamed": 1, "visible": True}
+
+# Alerts Button 
+reset_All_BransonPrimaryButton = {"checkable": False, "container": uIController_Overlay, "id": "resetAllBtn", "text": "Reset All", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+imageCross_Image = {"container": uIController_Overlay, "id": "imageCross", "source": "qrc:/Images/crossMark.svg", "type": "Image", "unnamed": 1, "visible": True}
+
+
+# Dashboard objects
+productionWindowItem_Item = {"container": uIController_QQuickWindowQmlImpl, "id": "productionWindowItem", "type": "Item", "unnamed": 1, "visible": True}
+# productionWindowItem_rectActionRecipeTitle_Rectangle = {"container": uIController_productionWindowItem_Item, "id": "rectActionRecipeTitle", "type": "Rectangle", "unnamed": 1, "visible": True}
+# productionWindowItem_Cycle_Count_Text = {"container": uIController_productionWindowItem_Item, "text": "Cycle Count", "type": "Text", "unnamed": 1, "visible": True}
+# productionWindowItem_Total_Cycle_Time_s_Text = {"container": uIController_productionWindowItem_Item, "text": "Total Cycle Time (s)", "type": "Text", "unnamed": 1, "visible": True}
+# imageLeftMenu_Image = {"container": uIController_QQuickWindowQmlImpl, "id": "imageLeftMenu", "source": "qrc:/Images/LeftMenu.svg", "type": "Image", "unnamed": 1, "visible": True}
+# RECIPES_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "RECIPES", "type": "Text", "unnamed": 1, "visible": True}
+# Overlay = {"container": uIController_QQuickWindowQmlImpl, "type": "Overlay", "unnamed": 1, "visible": True}
+# imageCross_Image = {"container": uIController_Overlay, "id": "imageCross", "source": "qrc:/Images/crossMark.svg", "type": "Image", "unnamed": 1, "visible": True}
+# OK_BransonPrimaryButton = {"checkable": False, "container": uIController_QQuickWindowQmlImpl, "id": "ok", "text": "OK", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+# imageConnectedNotification_Image = {"container": uIController_QQuickWindowQmlImpl, "id": "imageConnectedNotification", "source": "qrc:/Images/Connected.svg", "type": "Image", "unnamed": 1, "visible": True}
+# backGround_Rectangle = {"container": uIController_QQuickWindowQmlImpl, "id": "backGround", "type": "Rectangle", "unnamed": 1, "visible": True}
+# reset_All_BransonPrimaryButton = {"checkable": False, "container": uIController_Overlay, "id": "resetAllBtn", "text": "Reset All", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+# imageRightMenu_Image = {"container": uIController_QQuickWindowQmlImpl, "id": "imageRightMenu", "source": "qrc:/Images/RightMenu.svg", "type": "Image", "unnamed": 1, "visible": True}
+# mainWindowOpacity_Rectangle = {"container": uIController_QQuickWindowQmlImpl, "id": "mainWindowOpacity", "type": "Rectangle", "unnamed": 1, "visible": True}
+# welderSelComboBox_BransonComboBox = {"container": uIController_QQuickWindowQmlImpl, "id": "welderSelComboBox", "type": "BransonComboBox", "unnamed": 1, "visible": True}
+# manageImageicon_Image = {"container": uIController_QQuickWindowQmlImpl, "id": "manageImageicon", "source": "qrc:/Images/system.svg", "type": "Image", "unnamed": 1, "visible": True}
+# oK_BransonPrimaryButton = {"checkable": False, "container": uIController_Overlay, "id": "resetButton", "text": "OK", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+# canvash_CanvasItem = {"container": uIController_QQuickWindowQmlImpl, "id": "canvash", "type": "CanvasItem", "unnamed": 1, "visible": True}
+# PRODUCTION_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "PRODUCTION", "type": "Text", "unnamed": 1, "visible": True}
+# ANALYTICS_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "ANALYTICS", "type": "Text", "unnamed": 1, "visible": True}
+# SYSTEM_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "SYSTEM", "type": "Text", "unnamed": 1, "visible": True}
+# productionWindowItem_Active_Recipe_Text = {"container": uIController_productionWindowItem_Item, "text": "Active Recipe", "type": "Text", "unnamed": 1, "visible": True}
+# productionWindowItem_rectCycleCountTitle_Rectangle = {"container": uIController_productionWindowItem_Item, "id": "rectCycleCountTitle", "type": "Rectangle", "unnamed": 1, "visible": True}
+# alarmNum_Rectangle = {"container": uIController_QQuickWindowQmlImpl, "id": "alarmNum", "type": "Rectangle", "unnamed": 1, "visible": True}
+# imageAlarmNotification_Image = {"container": uIController_QQuickWindowQmlImpl, "id": "imageAlarmNotification", "source": "qrc:/Images/AlarmNotification.svg", "type": "Image", "unnamed": 1, "visible": True}
+# Logged_in_as_ADMIN_Executive_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Logged in as ADMIN (Executive)", "type": "Text", "unnamed": 1, "visible": True}
+# Actuator_Setup_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Actuator Setup", "type": "Text", "unnamed": 1, "visible": True}
+# imagePanIcon_Image = {"container": uIController_QQuickWindowQmlImpl, "id": "imagePanIcon", "source": "qrc:/Images/Pen.svg", "type": "Image", "unnamed": 1, "visible": True}
+# Diagnostics_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Diagnostics", "type": "Text", "unnamed": 1, "visible": True}
+# Import_Export_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Import/ Export", "type": "Text", "unnamed": 1, "visible": True}
+# systemOptionImage_Image = {"container": uIController_QQuickWindowQmlImpl, "id": "systemOptionImage", "source": "qrc:/Images/Language2.svg", "type": "Image", "unnamed": 1, "visible": True}
+# SAVE_BransonPrimaryButton = {"checkable": False, "container": uIController_QQuickWindowQmlImpl, "id": "saveButton", "text": "SAVE", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+# Metric_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Metric", "type": "Text", "unnamed": 1, "visible": True}
+# CANCEL_BransonPrimaryButton = {"checkable": False, "container": uIController_QQuickWindowQmlImpl, "id": "cancel", "text": "CANCEL", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+# systemOptionImage_Image_2 = {"container": uIController_QQuickWindowQmlImpl, "id": "systemOptionImage", "source": "qrc:/Images/Change_password_icon.svg", "type": "Image", "unnamed": 1, "visible": True}
+# resetPasswordTextRect_Rectangle = {"container": uIController_Overlay, "id": "resetPasswordTextRect", "type": "Rectangle", "unnamed": 1, "visible": True}
+# popupClose_Image = {"container": uIController_Overlay, "id": "popupClose", "source": "qrc:/Images/crossMark.svg", "type": "Image", "unnamed": 1, "visible": True}
+# systemOptionImage_Image_3 = {"container": uIController_QQuickWindowQmlImpl, "id": "systemOptionImage", "source": "qrc:/Images/Logout Icon.svg", "type": "Image", "unnamed": 1, "visible": True}
+# No_BransonPrimaryButton = {"checkable": False, "container": uIController_QQuickWindowQmlImpl, "id": "cancel", "text": "No", "type": "BransonPrimaryButton", "unnamed": 1, "visible": True}
+# Fieldbus_Token_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Fieldbus Token", "type": "Text", "unnamed": 1, "visible": True}
+# Ethernet_Connected_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "Ethernet Connected", "type": "Text", "unnamed": 1, "visible": True}
+# USB_Plugged_In_Text = {"container": uIController_QQuickWindowQmlImpl, "text": "USB Plugged-In", "type": "Text", "unnamed": 1, "visible": True}
+
+
+
+
+
+
