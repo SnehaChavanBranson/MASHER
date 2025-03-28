753a4b17a2c73ee27b9457cffc2c040888c03225
SnehaChavanBranson <sneha.chavan@emerson.com>
2025-03-28 12:47:38 +0530

    [add] testcase added for login

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,180 +1,110 @@
-# Author - Sneha Chavan
-# Created Date - <Agreed upon Date format>
-# Updated Date - <Agreed upon Date format>
-# Last Review Date - <Agreed upon Date format>
-# Defects logged - <List of all Defect IDs active or inactive>
-# Active Defects - <List of all active defects>
-
+# -*- coding: utf-8 -*-
 import squish
-import test
+from screens import login, dashboard
+from helper import core
 
-EXPECTED_BUILD_NAME = ""
-USERNAME = ""
-PASSWORD = ""
-VALUE_ACTUATOR = "Actuator"
-SUPPORTED_FEATURE_VALUE = "Others"
-CONTROLLER_SOFTWARE_VERSION = ""
-ACTUATOR_SOFTWARE_VERSION = ""
-SUPERVISORY_SOFTWARE_VERSION = ""
-POWER_CONTROLLER_SOFTWARE_VERSION = ""
+USERNAME = "ADMIN"
+PASSWORD = "Branson@2"
+EMPTY_PASSWORD = ""
+EMPTY_USERNAME = ""
+INCORRECT_USERNAME = "branson"
+INCORRECT_PASSWORD = "emerson@1"
 
+def CheckIfALertsbox():
+    login.AlertsButtonClick()
+        
+def test_positive_scenario():
+    """
+        This testcase is for login positive test case scenario:
+        
+        Description* Login with the Executive/Admin user with below credentials:
+        Username : ADMIN
+        Password: Branson@2
+    """
+    test.startSection("test case 1: positive test scenario with valid credentials")
+    snooze(2)
+    test.log(f'Step 1: Enter username {USERNAME}')
+    login.EnterUsername(USERNAME)
+    test.log(f'Step 2: Enter password {PASSWORD}')
+    login.EnterPassword(PASSWORD)
+    test.log("Step 3: Click on Login button.")
+    login.ClickLoginButton()
+    dashboard.WaitTillDashboardIsLoaded()
+    test.log("Expected Result: Admin user with valid credentials shall get logged in to the system")
+    snooze(2)
+    test.endSection()
+    
+def test_password_empty():
+    test.startSection("test case 2: password empty")
+    snooze(2)
+    login.EnterUsername(USERNAME)
+    test.log("entered into enter username")
+    login.EnterPassword(EMPTY_PASSWORD)
+    login.ClickLoginButton()
+    login.VerifyPopEmptyPassword()
+    login.ClickOkayPopUp()
+    snooze(2)
+    test.endSection()
+    
+def test_username_empty():
+    test.startSection("test case 3: username empty")
+    snooze(2)
+    login.EnterUsername(EMPTY_USERNAME)
+    test.log("entered empty username.")
+    login.EnterPassword(PASSWORD)
+    login.ClickLoginButton()
+    login.VerifyPopEmptyUsername()
+    login.ClickOkayPopUp()
+    snooze(2)
+    test.endSection()
 
-def main() -> None:
-    test.log(
-        """Step 1: Description*Record the Build Name of the software loaded onto machine."""
-    )
+def test_incorrect_username():
+    test.startSection("test case 4: incorrect username")
+    snooze(2)
+    login.EnterUsername(INCORRECT_USERNAME)
+    test.log("entered incorrect username.")
+    login.EnterPassword(PASSWORD)
+    login.ClickLoginButton()
+    login.VerifyInvalidCredsMessage()
+    login.ClickOkayPopUp()
+    snooze(2)
+    test.endSection()
 
-    build_name = GetBuildName()
+def test_incorrect_password():
+    test.startSection("test case 5: incorrect password")
+    snooze(2)
+    login.EnterUsername(USERNAME)
+    test.log("entered incorrect password.")
+    login.EnterPassword(INCORRECT_PASSWORD)
+    login.ClickLoginButton()
+    login.VerifyInvalidCredsMessage()
+    login.ClickOkayPopUp()
+    snooze(2)
+    test.endSection()
 
-    test.log(
-        """Expected Results:The Build Name is the same as intended for verification test."""
-    )
-    CheckIfBuildNameIsSame(build_name, EXPECTED_BUILD_NAME)
-
-    test.log("""Step 2: Description*Login to the UI""")
-    Login(username=USERNAME, password=PASSWORD)
-    WaitTillLandingPageIsLoaded()
-
-    test.log("""Expected Results:User login is successful.""")
-    CheckLoggedInUserName(USERNAME)
-
-    test.log(
-        """Step 3.1: Description*On the UIController software of HMI:
-    Navigate to Left menu -> SYSTEM --> INFORMATION"""
-    )
-    OpenMenuItem(LEFT_MENU_OBJECT, ["SYSTEM", "INFORMATION"])
-
-    test.log(
-        """Expected Results: Information tab on the Configuration tab of System screen shall be displayed successfully."""
-    )
-    CheckPresenceOfInformationTab()
-
-    test.log("""Step 3.2: Description*Check the Model value""")
-    model_value = GetInformationTabModelValue()
-
-    test.log("""Expected Results: Model shall have value as Actuator Type""")
-    CheckIfValueIsSame(model_value, VALUE_ACTUATOR)
-
-    test.log(
-        """Step 3.3: Description*On the UIController software of HMI: Navigate to Left menu -> SYSTEM --> INFORMATION -->System Feature"""
-    )
-    OpenMenuItem(LEFT_MENU_OBJECT, ["SYSTEM", "INFORMATION", "System Feature"])
-
-    test.log(
-        """Expected Results: Information tab on the Configuration tab of System screen shall be displayed successfully."""
-    )
-    CheckPresenceOfInformationTab()
-
-    test.log("""Step 3.4: Description*Check the other supported features.""")
-    supported_features = GetSupportedFeaturesInInformationTab()
-
-    test.log("""Expected Results: Model shall have value as Others""")
-    CheckIfValueIsSame(supported_features, SUPPORTED_FEATURE_VALUE)
-
-    test.log("""Step 4: Description*Navigate to Left menu -> SYSTEM -> INFORMATION""")
-    OpenMenuItem(LEFT_MENU_OBJECT, ["SYSTEM", "INFORMATION"])
-
-    test.log(
-        """Expected Results: Machine Details tab in the INFORMATION page is displayed."""
-    )
-    CheckPresenceOfMachineDetailsTab()
-
-    test.log(
-        """Step 5: Description*Record UI Controller Software Version, as displayed by the UI."""
-    )
-    controller_version = GetControllerSoftwareVersion()
-    get_stream = GetApplicationBuildStream()
-
-    test.log(
-        """Expected Results:
-    • If the version under test is from the Main Stream,
-    recorded version number consists of four groups of digits separated by "."
-    • If the version under test is from the Integration Stream,
-    recorded version number consists of three groups of digits separated by "."
-    • Recorded version number corresponds to software build intended for test."""
-    )
-
-    CheckStreamVersionDigitGroup(get_stream)
-    CheckIfValueIsSame(controller_version, CONTROLLER_SOFTWARE_VERSION)
-
-    test.log(
-        """Step 6: Description*Record Actuator Controller Software Version, as displayed by the UI."""
-    )
-    actuator_version = GetActuatorSoftwareVersion()
-
-    test.log(
-        """Expected Results:
-    • If the version under test is from the Main Stream,
-    recorded version number consists of four groups of digits separated by "."
-    • If the version under test is from the Integration Stream,
-    recorded version number consists of three groups of digits separated by "."
-    • Recorded version number corresponds to software build intended for test."""
-    )
-
-    CheckStreamVersionDigitGroup(get_stream)
-    CheckIfValueIsSame(actuator_version, ACTUATOR_SOFTWARE_VERSION)
-
-    test.log(
-        """Step 7: Description*Record Supervisory Controller Software Version, as displayed by the UI."""
-    )
-    supervisory_version = GetSupervisorySoftwareVersion()
-
-    test.log(
-        """Expected Results:
-    • If the version under test is from the Main Stream,
-    recorded version number consists of four groups of digits separated by "."
-    • If the version under test is from the Integration Stream,
-    recorded version number consists of three groups of digits separated by "."""
-    )
-    CheckStreamVersionDigitGroup(get_stream)
-    CheckIfValueIsSame(supervisory_version, SUPERVISORY_SOFTWARE_VERSION)
-
-    test.log(
-        """Step 8: Description*Record Power Controller Software Version, as displayed by the UI."""
-    )
-    power_controller_version = GetPowerControllerSoftwareVersion()
-
-    test.log(
-        """Expected Results:
-    • If the version under test is from the Main Stream,
-    recorded version number consists of four groups of digits separated by "."
-    • If the version under test is from the Integration Stream,
-    recorded version number consists of three groups of digits separated by "."
-    • Recorded version number corresponds to software build intended for test."""
-    )
-
-    CheckStreamVersionDigitGroup(get_stream)
-    CheckIfValueIsSame(power_controller_version, POWER_CONTROLLER_SOFTWARE_VERSION)
-
-    test.log(
-        """Step 9: Description*• Navigate from Dashboard to Left menu -> SYSTEM -> CONFIGURATION -> GENERAL
-    • Record MACHINE NAME"""
-    )
-
-    OpenMenuItem(LEFT_MENU_OBJECT, ["SYSTEM", "CONFIGURATION", "GENERAL"])
-    machine_name = GetMachineName()
-
-    test.log("""Expected Results: MACHINE NAME is recorded.""")
-    CheckIfValueIsNotEmpty(machine_name)
-
-    test.log(
-        """Step 10: Description*• Navigate from Dashboard to Left menu -> SYSTEM -> INFORMATION -> Event Log"""
-    )
-
-    OpenMenuItem(LEFT_MENU_OBJECT, ["SYSTEM", "INFORMATION", "Event Log"])
-
-    test.log(
-        """Expected Results: Events named "SOFTWARE VERSION CHANGE" show values in the 'To' column that identical to values recorded in steps 4, 5, and 6."""
-    )
-    CheckEventLogValues(controller_version)
-    CheckEventLogValues(actuator_version)
-    CheckEventLogValues(supervisory_version)
-    CheckEventLogValues(power_controller_version)
-
-
-if __name__ == "__main__":
-    ExecutePreConditions()  # Ensure the system is in known ideal state before execution
-    StartApplication()  # Start the AUT
-    main()  # The main body corresponding to the Testcase.
-    StopApplication()  # Stop the AUT
-    ExecutePostConditions()  # Ensure the system is reverted to known ideal state post execution
+def test_incorrect_credentials():
+    test.startSection("test case 6: incorrect credentials")
+    snooze(2)
+    test.log("entered incorrect username and password.")
+    login.EnterUsername(INCORRECT_USERNAME)
+    login.EnterPassword(INCORRECT_PASSWORD)
+    login.ClickLoginButton()
+    login.VerifyInvalidCredsMessage()
+    login.ClickOkayPopUp()
+    snooze(2)
+    test.endSection()
+    
+def main():
+    test.log("enter main")
+    startApplication("QT_UIController")
+    test_username_empty()
+    # test_password_empty()
+    test_incorrect_username()
+    # test_incorrect_password()
+    # test_incorrect_credentials()
+    test_positive_scenario()
+    
+    
+    
+    
+    
